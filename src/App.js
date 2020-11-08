a008bc6643e6e552d880ba4c3cd7302dd08a0cb9
duer <duqingbetter@qq.com>
2020-11-09 00:41:17 +0800

    已添加        .gitignore 已添加        README.md 已添加        package-lock.json 已添加        package.json 已添加        public/favicon.ico 已添加        public/index.html 已添加        public/logo192.png 已添加        public/logo512.png 已添加        public/manifest.json 已添加        public/robots.txt 已添加        src/App - 副本.js 已添加        src/App.css 已添加        src/App.js 已添加        src/App.test.js 已添加        src/index.css 已添加        src/index.js 已添加        src/logo.svg 已添加        src/reportWebVitals.js 已添加        src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
