6f66cf3a851910b76c01a5c05ad4c3f41b33586f
duer <duqingbetter@qq.com>
2020-11-09 01:14:22 +0800

    已修改        src/App.js 已修改        src/index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import './App.css';
-function App() {
+/* function App() {
   return (
     <div className="App">
       <h1>Hello, world!</h1>
       <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+} */
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>现在是 {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App date={new Date()} />
   </React.StrictMode>,
   document.getElementById('root')
 );
